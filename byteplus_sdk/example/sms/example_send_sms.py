dc003bd816372d194920de4793167cdb7222185c
chenxin.shayne <chenxin.shayne@bytedance.com>
2022-02-15 14:34:13 +0800

    code for sms
    Change-Id: Ib1e015180e62e8310d72bc11999430596d97c03c

---
+++ byteplus_sdk/example/sms/example_send_sms.py
@@ -0,0 +1,56 @@
+# coding:utf-8
+from __future__ import print_function
+
+import json
+
+from byteplus_sdk.sms.SmsService import SmsService
+from byteplus_sdk.const.Const import *
+
+if __name__ == '__main__':
+    # oversea
+    sms_service = SmsService(REGION_AP_SINGAPORE1)
+
+    # cn
+    # sms_service = SmsService()
+
+    # call below method if you dont set ak and sk in $HOME/.volc/config
+    sms_service.set_ak('ak')
+    sms_service.set_sk('sk')
+    # sms_service.set_host('host')
+
+    body = {
+        "SmsAccount": "smsAccount",
+        "Sign": "senderid",
+        "TemplateID": "ST_xxx",
+        "TemplateParam": "{\"code\": \"111\"}",
+        "PhoneNumbers": "+65xxxxxxxx",
+        "Tag": "tag",
+    }
+
+    body = json.dumps(body)
+    resp = sms_service.send_sms(body)
+    print(resp)
+
+    body = {
+        "SmsAccount" : "smsAccount",
+        "Sign" : "senderid",
+        "TemplateID": "ST_xxx",
+        "PhoneNumber": "+65xxxxxxxx",
+        "CodeType": 6,
+        "TryCount": 3,
+        "ExpireTime": 240,
+        "Scene": "Test"
+    }
+    body = json.dumps(body)
+    resp = sms_service.send_sms_verify_code(body)
+    print(resp)
+
+    body = {
+        "SmsAccount": "smsAccount",
+        "PhoneNumber": "+65xxxxxxxx",
+        "Scene": "Test",
+        "Code" : "123456"
+    }
+    body = json.dumps(body)
+    resp = sms_service.check_sms_verify_code(body)
+    print(resp)
