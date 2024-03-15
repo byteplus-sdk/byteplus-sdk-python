a7be779869c917658d159ff29b0d07f8d0baf477
fengxiaodong.fxd <fengxiaodong.fxd@bytedance.com>
2024-03-15 15:38:50 +1100

    feat: submitSDK_MR

---
+++ byteplus_sdk/example/live/v20180101/DeleteCasterResourceOPEDExample.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+from byteplus_sdk.live.v20180101.live_service import LiveService
+
+if __name__ == '__main__':
+    service = LiveService()
+
+    # call below method if you dont set ak and sk in $HOME/.byteplus/config
+    service.set_ak('ak')
+    service.set_sk('sk')
+
+    body = {}
+
+    resp = service.delete_caster_resource_oped(body)
+    print(resp)
